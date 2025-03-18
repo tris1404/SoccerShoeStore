a82c3a97f88b32d623d2e6b2d21574b1936532aa
Nguyen Tai Tri <trint3445@ut.edu.vn>
2025-03-18 15:52:54 +0700

    docs: add home page interface

--- SoccerShoeStore/public/index.php
+++ SoccerShoeStore/public/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/css/styles.css" type="text/css">
+    <link rel="stylesheet" href="assets/css/styles.css?v=1" type="text/css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <title>Soccer Shoes Store</title>
 </head>
@@ -24,51 +24,154 @@
                         <img src="assets/img/banner/puma.png" alt="">
                         <img src="assets/img/banner/mizuno.jpg" alt="">
                         <img src="assets/img/banner/kamito.png" alt="">
-                   </div>
+                    </div>
+                    <div class="btns">
+                        <div class="btn-left"><i class="fa-solid fa-angle-left"></i></div>
+                        <div class="btn-right"><i class="fa-solid fa-angle-right"></i></div>
+                    </div>
+                    <div class="index-img">
+                        <div class="index-item index-item-0 active"></div>
+                        <div class="index-item index-item-1"></div>
+                        <div class="index-item index-item-2"></div>
+                        <div class="index-item index-item-3"></div>
+                        <div class="index-item index-item-4"></div>
+                    </div>
                 </div>
                 <div class="mustbuy">
-                <div class="ega-container">
-                    <div class="mustbuy-title">
-                        <h2>Sản phẩm nên mua</h2>
+                    <div class="ega-container">
+                        <div class="mustbuy-title">
+                            <h2>BỘ SƯU TẬP MỚI</h2>
+                        </div>
+                        <div class="mustbuy-img">
+                            <a href="">
+                                <img src="assets/img/banner-title/nike.jpg" alt="">
+                            </a>
+                            <div class="mustbuy-product">
+                                <div class="mustbuy-item">
+                                    <span class="sale-tag">NEW</span>
+                                    <img src="https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/8e644f9b-4db8-4d24-91d3-1edf45ae1e3c/ZOOM+SUPERFLY+9+ELITE+MR+FG.png" alt="Nike Air Max">
+                                    <h3>Nike Zoom Mercurial Superfly 9 Elite 'Marcus Rashford'</h3>
+                                    <p class="price">Giá: 2.500.000 VNĐ</p>
+                                    <p>Giày thể thao phong cách, êm ái, phù hợp cho mọi hoạt động.</p>
+                                </div>
+
+                                <div class="mustbuy-item">
+                                    <span class="sale-tag">NEW</span>
+                                    <img src="https://product.hstatic.net/200000740801/product/lux_galaxy01015_ac8ffab58496489c889b55161689e6e6_master.jpg" alt="Nike ZoomX">
+                                    <h3>PUMA ULTRA 5 MATCH VOL. UP TT</h3>
+                                    <p class="price">Giá: 3.200.000 VNĐ</p>
+                                    <p>Thiết kế tối ưu cho chạy bộ, độ bám tốt, trọng lượng nhẹ.</p>
+                                </div>
+
+                                <div class="mustbuy-item">
+                                    <span class="sale-tag">NEW</span>
+                                    <img src="https://assets.adidas.com/images/w_600,f_auto,q_auto/74600fef74e8434ba613699779d2f806_9366/Giay_DJa_Bong_Turf_F50_League_trang_IE1231_22_model.jpg" alt="Nike Air Force 1">
+                                    <h3>ADIDAS F50 PRO TF - IE1220 - TRẮNG/ĐỎ</h3>
+                                    <p class="price">Giá: 2.800.000 VNĐ</p>
+                                    <p>Giày sneaker huyền thoại, phối đồ đa dạng, phù hợp mọi phong cách.</p>
+                                </div>
+
+                                <div class="mustbuy-item">
+                                    <span class="sale-tag">NEW</span>
+                                    <img src="https://product.hstatic.net/1000313927/product/sh_p1ga236009_06_d1246c9c1033489ab7787adc851cc503_large.jpg" alt="Nike Air Force 1">
+                                    <h3>MIZUNO A JAPAN</h3>
+                                    <p class="price">Giá: 2.800.000 VNĐ</p>
+                                    <p>Giày sneaker huyền thoại, phối đồ đa dạng, phù hợp mọi phong cách.</p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div class="mustbuy-img">
-                        <a href="">
-                            <img src="assets/img/banner-title/nike.jpg" alt="">
-                        </a>
+                </div>
+                <div class="care-about">
+                    <div class="ega-container">
+                        <div class="care-about-title">
+                            <h2>BẠN ĐANG QUAN TÂM ĐẾN - ƯU ĐÃI HẤP DẪN</h2>
+                        </div>
+                        <div class="care-about-img">
+                            <a href="">
+                                <img src="" alt="">
+                            </a>
+                            <div class="mustbuy-product">
+                                <div class="mustbuy-item">
+                                    <img src="https://theme.hstatic.net/200000278317/1000929405/14/newcoll_1_img_large.jpg?v=2118" alt="Nike Air Max">
+                                </div>
 
-                        <div class="mustbuy-product">
-                            <div class="mustbuy-item">
-                                <span class="sale-tag">NEW</span>
-                                <img src="https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/8e644f9b-4db8-4d24-91d3-1edf45ae1e3c/ZOOM+SUPERFLY+9+ELITE+MR+FG.png" alt="Nike Air Max">
-                                <h3>Nike Air Max</h3>
-                                <p class="price">Giá: 2.500.000 VNĐ</p>
-                                <p>Giày thể thao phong cách, êm ái, phù hợp cho mọi hoạt động.</p>
+                                <div class="mustbuy-item">
+                                    <img src="https://theme.hstatic.net/200000278317/1000929405/14/newcoll_2_img_large.jpg?v=2118" alt="Nike ZoomX">                                 
+                                </div>
+
+                                <div class="mustbuy-item">
+                                    <img src="https://theme.hstatic.net/200000278317/1000929405/14/newcoll_4_img_large.jpg?v=2118" alt="Nike Air Force 1">                                                                      
+                                </div>
+
+                                <div class="mustbuy-item">                                    
+                                    <img src="https://product.hstatic.net/1000313927/product/sh_p1ga236009_06_d1246c9c1033489ab7787adc851cc503_large.jpg" alt="Nike Air Force 1">
+                                </div>
                             </div>
+                        </div>
+                    </div>
+                </div>
 
-                            <div class="mustbuy-item">
-                                <span class="sale-tag">NEW</span>
-                                <img src="https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/acdcfcf6-4a6d-49a0-9eda-9e40b4990596/ZM+SUPERFLY+10+ELITE+FG+LV8.png" alt="Nike ZoomX">
-                                <h3>Nike ZoomX</h3>
-                                <p class="price">Giá: 3.200.000 VNĐ</p>
-                                <p>Thiết kế tối ưu cho chạy bộ, độ bám tốt, trọng lượng nhẹ.</p>
+                <div class="list-brand">
+                    <div class="ega-container">
+                        <div class="list-brand-title">
+                            <h2>THƯƠNG HIỆU</h2>
+                        </div>
+                        <div class="list-brand-img">
+                            <a href="">
+                                <img src="" alt="">
+                            </a>
+                            <div class="mustbuy-product">
+                                <div class="mustbuy-item">
+                                    <img src="https://theme.hstatic.net/200000278317/1000929405/14/check_use_icon_1_large.jpg?v=2118" alt="Nike Air Max">
+                                    <h3>NIKE</h3>
+                                </div>
+
+                                <div class="mustbuy-item">
+                                    <img src="https://theme.hstatic.net/200000278317/1000929405/14/check_use_icon_2_large.jpg?v=2118" alt="Nike ZoomX">                                 
+                                    <h3>ADIDAS</h3>
+                                </div>
+
+                                <div class="mustbuy-item">
+                                    <img src="https://theme.hstatic.net/200000278317/1000929405/14/check_use_icon_3_large.jpg?v=2118" alt="Nike Air Force 1">                                                                      
+                                    <h3>MIZUNO</h3>
+                                </div> 
+
+                                <div class="mustbuy-item">
+                                    <img src="https://theme.hstatic.net/200000278317/1000929405/14/check_use_icon_5_large.jpg?v=2118" alt="Nike Air Force 1">                                                                      
+                                    <h3>JOMA</h3>
+                                </div>
+
+                                <div class="mustbuy-item">
+                                    <img src="https://theme.hstatic.net/200000278317/1000929405/14/check_use_icon_4_large.jpg?v=2118" alt="Nike Air Force 1">                                                                      
+                                    <h3>PUMA</h3>
+                                </div>
+                                
+                                <div class="mustbuy-item">
+                                    <img src="https://theme.hstatic.net/200000278317/1000929405/14/check_use_icon_6_large.jpg?v=2118" alt="Nike Air Force 1">                                                                      
+                                    <h3>DESPORTE</h3>
+                                </div>
+
+                                <div class="mustbuy-item">
+                                    <img src="https://theme.hstatic.net/200000278317/1000929405/14/check_use_icon_8_large.jpg?v=2118" alt="Nike Air Force 1">                                                                      
+                                    <h3>KAMITO</h3>
+                                </div>
+
+                                <div class="mustbuy-item">
+                                    <img src="https://theme.hstatic.net/200000278317/1000929405/14/check_use_icon_7_large.jpg?v=2118" alt="Nike Air Force 1">                                                                      
+                                    <h3>ASICS</h3>
+                                </div>
+
+                                <div class="mustbuy-item">
+                                    <img src="https://theme.hstatic.net/200000278317/1000929405/14/check_use_icon_9_large.jpg?v=2118" alt="Nike Air Force 1">                                                                      
+                                    <h3>ZOCKER</h3>
+                                </div>
+
                             </div>
-
-                            <div class="mustbuy-item">
-                                <span class="sale-tag">NEW</span>
-                                <img src="https://assets.adidas.com/images/w_600,f_auto,q_auto/74600fef74e8434ba613699779d2f806_9366/Giay_DJa_Bong_Turf_F50_League_trang_IE1231_22_model.jpg" alt="Nike Air Force 1">
-                                <h3>Nike Air Force 1</h3>
-                                <p class="price">Giá: 2.800.000 VNĐ</p>
-                                <p>Giày sneaker huyền thoại, phối đồ đa dạng, phù hợp mọi phong cách.</p>
-                            </div>
-
-                            
-
-                           
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
             <!-- End content -->
 
             <!-- Sidebar -->
@@ -81,6 +184,6 @@
         <!-- End footer -->
     </div>
 
-    <script src="assets/js/scripts.js"></script>
+    <script src="assets/js/scripts.js?v=1"></script>
 </body>
 </html>
