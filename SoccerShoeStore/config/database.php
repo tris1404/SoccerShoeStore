e22e09233c723f2a79e7bda1af87e8ebe5c1ea4d
TrongDang2608 <dangtrong2608@gmail.com>
2025-03-28 21:31:01 +0700

    update database.php.php
    Signed-off-by: TrongDang2608 <dangtrong2608@gmail.com>

--- SoccerShoeStore/config/database.php
+++ SoccerShoeStore/config/database.php
@@ -2,7 +2,7 @@
   $db_server = "localhost";
   $db_user = "root";
   $db_pass = "";
-  $db_name = "soccershoestore";
+  $db_name = "shoe_store";
 
 
   $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
