fe826ece8604a2294d5096d774952c2edaeb2ccd
TrongDang2608 <dangtrong2608@gmail.com>
2025-03-31 07:27:26 +0700

    đổi tên database.php
    Signed-off-by: TrongDang2608 <dangtrong2608@gmail.com>

--- SoccerShoeStore/config/database.php
+++ SoccerShoeStore/config/database.php
@@ -2,7 +2,7 @@
   $db_server = "localhost";
   $db_user = "root";
   $db_pass = "";
-  $db_name = "shoe_store";
+  $db_name = "soccershoestore";
 
 
   $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
